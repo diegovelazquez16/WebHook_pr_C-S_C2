d89adce661209a275e2fd1a427e1481c4f449d93
Diego Velazquez <govelazmen@gmail.com>
2025-02-24 20:27:20 -0600

    Put: entidad user

--- pull_request_webhook/domain/value_objects/payload_structures.go
+++ pull_request_webhook/domain/value_objects/payload_structures.go
@@ -3,6 +3,7 @@ package value_objects
 type PullRequestEvent struct {
 	Action      string      `json:"action"`
 	PullRequest PullRequest `json:"pull_request"`
+	Repository  Repository  `json:"repository"`
 }
 
 type PullRequest struct {
@@ -11,9 +12,18 @@ type PullRequest struct {
 	Head  Branch `json:"head"`
 	Base  Branch `json:"base"`
 	URL   string `json:"url"`
+	User  User   `json:"user"`
 }
 
 type Branch struct {
 	Ref string `json:"ref"`
 	SHA string `json:"sha"`
+}
+
+type User struct {
+	UserName string `json:"login"`
+}
+
+type Repository struct {
+	Name string `json:"name"`
 }
