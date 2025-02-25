b1639c16cbf339cb7854e2d3d7959df5d94fd512
Diego Velazquez <govelazmen@gmail.com>
2025-02-25 16:42:36 -0600

    General fix

--- pull_request_webhook/infrastructure/routes.go
+++ pull_request_webhook/infrastructure/routes.go
@@ -3,13 +3,13 @@ package infrastructure
 import "github.com/gin-gonic/gin"
 
 func Routes(router *gin.Engine) {
-
-	// https://38a1-189-150-46-82.ngrok-free.app/pull_request/process
-
 	routes := router.Group("pull_request")
-
 	{
 		routes.POST("/process", HandlePullRequestEvent)
 	}
 
+	router.POST("/", func(ctx *gin.Context) {
+		ctx.Request.URL.Path = "/pull_request/process"
+		router.HandleContext(ctx)
+	})
 }
